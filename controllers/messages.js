73f1383dc34713c7a83e7bff6180be7bee2f568b
dear17530 <dear17530@gmail.com>
2021-08-24 14:54:33 +0800

    聊天室新增自動回覆

--- controllers/messages.js
+++ controllers/messages.js
@@ -11,6 +11,63 @@ export const sendMessage = async (req, res) => {
       date: new Date()
     })
     res.status(200).send({ success: true, message: '', result })
+    if (req.body.message === '我該如何發起活動？') {
+      await messages.create({
+        message: '您可以至活動頁面點擊「新增活動」。',
+        welcomeMessage: false,
+        sender: mongoose.Types.ObjectId('60f52553a2ad5002acfd3675'),
+        receiver: mongoose.Types.ObjectId(req.user._id),
+        date: new Date()
+      })
+    } else if (req.body.message === '我該如何編輯活動？') {
+      await messages.create({
+        message: '發起活動後，您可以至「會員中心」點擊編輯活動進行編輯。',
+        welcomeMessage: false,
+        sender: mongoose.Types.ObjectId('60f52553a2ad5002acfd3675'),
+        receiver: mongoose.Types.ObjectId(req.user._id),
+        date: new Date()
+      })
+    } else if (req.body.message === '我該如何查看有誰參與了我的活動？') {
+      await messages.create({
+        message: '發起活動後，您可以至「會員中心」查看該活動的跟團者。',
+        welcomeMessage: false,
+        sender: mongoose.Types.ObjectId('60f52553a2ad5002acfd3675'),
+        receiver: mongoose.Types.ObjectId(req.user._id),
+        date: new Date()
+      })
+    } else if (req.body.message === '我該如何參與他人的活動？') {
+      await messages.create({
+        message: '您可以至活動頁面點擊「參加」。',
+        welcomeMessage: false,
+        sender: mongoose.Types.ObjectId('60f52553a2ad5002acfd3675'),
+        receiver: mongoose.Types.ObjectId(req.user._id),
+        date: new Date()
+      })
+    } else if (req.body.message === '我該如何私訊主辦人？') {
+      await messages.create({
+        message: '至活動頁面點擊「私訊」後查看您的「私人訊息」與主辦人聯繫。',
+        welcomeMessage: false,
+        sender: mongoose.Types.ObjectId('60f52553a2ad5002acfd3675'),
+        receiver: mongoose.Types.ObjectId(req.user._id),
+        date: new Date()
+      })
+    } else if (req.body.message === '回饋點數可以怎麼使用？') {
+      await messages.create({
+        message: '結帳時，輸入您要折抵的點數，注意單筆訂單最多只能使用 50 點。',
+        welcomeMessage: false,
+        sender: mongoose.Types.ObjectId('60f52553a2ad5002acfd3675'),
+        receiver: mongoose.Types.ObjectId(req.user._id),
+        date: new Date()
+      })
+    } else if (req.body.message === '回饋點數怎麼獲得？') {
+      await messages.create({
+        message: '商品達優惠門檻後，管理員會立即發放優惠點數。',
+        welcomeMessage: false,
+        sender: mongoose.Types.ObjectId('60f52553a2ad5002acfd3675'),
+        receiver: mongoose.Types.ObjectId(req.user._id),
+        date: new Date()
+      })
+    }
   } catch (error) {
     console.log(error)
     res.status(500).send({ success: false, message: '伺服器錯誤' })
@@ -67,7 +124,8 @@ export const getMessage = async (req, res) => {
           'receiver.imagefiles': 1,
           'receiver.role': 1,
           'receiver.userId': 1,
-          date: 1
+          date: 1,
+          welcomeMessage: 1
         }
       }, {
         $sort: {
