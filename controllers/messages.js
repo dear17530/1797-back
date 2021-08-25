ebdb88bb3a020acd38ee4b873217d702ca7173a8
dear17530 <dear17530@gmail.com>
2021-08-25 15:23:43 +0800

    取得全部訊息

--- controllers/messages.js
+++ controllers/messages.js
@@ -140,6 +140,64 @@ export const getMessage = async (req, res) => {
   }
 }
 
+export const getAllMessage = async (req, res) => {
+  try {
+    const result = await messages.aggregate([
+      {
+        $match: {
+          receiver: mongoose.Types.ObjectId(req.params.id)
+        }
+      }, {
+        $lookup: {
+          from: 'users',
+          localField: 'sender',
+          foreignField: '_id',
+          as: 'sender'
+        }
+      }, {
+        $lookup: {
+          from: 'users',
+          localField: 'receiver',
+          foreignField: '_id',
+          as: 'receiver'
+        }
+      }, {
+        $unwind: {
+          path: '$receiver'
+        }
+      }, {
+        $unwind: {
+          path: '$sender'
+        }
+      }, {
+        $project: {
+          message: 1,
+          'sender._id': 1,
+          'sender.avatar': 1,
+          'sender.imagefiles': 1,
+          'sender.role': 1,
+          'sender.userId': 1,
+          'receiver._id': 1,
+          'receiver.avatar': 1,
+          'receiver.imagefiles': 1,
+          'receiver.role': 1,
+          'receiver.userId': 1,
+          date: 1,
+          welcomeMessage: 1
+        }
+      }, {
+        $sort: {
+          date: 1
+        }
+      }
+    ])
+    res.status(200).send({ success: true, message: '', result })
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({ success: false, message: '伺服器錯誤' })
+  }
+}
+
 export const getProfile = async (req, res) => {
   try {
     const result = await users.aggregate([
--- routes/messages.js
+++ routes/messages.js
@@ -4,13 +4,15 @@ import auth from '../middleware/auth.js'
 import {
   sendMessage,
   getMessage,
-  getProfile
+  getProfile,
+  getAllMessage
 } from '../controllers/messages.js'
 
 const router = express.Router()
 
 router.post('/', auth, sendMessage)
 router.get('/profile/:id', auth, getProfile)
+router.get('/allMessages/:id', auth, getAllMessage)
 router.get('/:id', auth, getMessage)
 
 export default router
